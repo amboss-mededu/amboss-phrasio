ae63025c6320832e4c69e19b3a98259dfc867485
ajt <alexjeffcott@gmail.com>
2022-02-02 15:03:34 +0100

    Adjust adaptor

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,3 @@
-export const getTooltipContent = (locale, contentId) =>
-    window.ambossAnnotationAdaptor({
-        subject: "getTooltipContent",
-        contentId,
-        locale
-    });
-
-export const track = (name, args) =>
-  window.ambossAnnotationAdaptor({
-    subject: "track",
-    trackingProperties: [name, args],
-  });
-
 export function loadFonts() {
   const lato700 = new FontFace("Lato", "/fonts/Lato.700.normal.woff2");
   document.fonts.add(lato700);
